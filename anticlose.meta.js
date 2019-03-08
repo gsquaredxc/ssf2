01af85848c178335b8cac41b3111c8b10225f40f
gsquaredxc <gsquaredxc@users.noreply.github.com>
2019-03-08 11:07:20 -0600

    Update anticlose.meta.js

--- anticlose.meta.js
+++ anticlose.meta.js
@@ -1,1 +1,11 @@
-
+// ==UserScript==
+// @name         New Userscript
+// @namespace    tampermonkey.com
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @match        www.mcleodgaming.com/games/ssf2
+// @update       https://github.com/gsquaredxc/ssf2/edit/master/anticlose.meta.js
+// @download     https://github.com/gsquaredxc/ssf2/edit/master/anticlose.user.js
+// @grant        none
+// ==/UserScript==
